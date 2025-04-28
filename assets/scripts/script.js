4dc79a3fa8ee52bb9787ec5a266f91eb2710ff5f
JoFaTech2508 <johfaa123@gmail.com>
2025-04-29 00:05:10 +0200

    added some comments in JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
-// Script for waterdrop
+// Script for waterdrop (scroll-activated)
+// Makes the water drop icon to add/remove the "splash" effect, when you scroll down past 100 pixels.
 const waterIcon = document.querySelector('.section-top__icon');
 window.addEventListener('scroll', () => {
   waterIcon.classList.toggle('splash', window.scrollY > 100);
@@ -6,6 +7,8 @@ window.addEventListener('scroll', () => {
 
 
 // Script for section 2 -  Wave text
+/* Uses Intersection Observer to make text appear/disappear by adjusting font size as the section enters/exits the viewport 
+(with a 20% scroll buffer)*/
 const waveSectionText = document.querySelector(".section-wave__text");
 const originalFontSize = window.getComputedStyle(waveSectionText).fontSize;
 const observerWaveText = new IntersectionObserver(
@@ -20,6 +23,7 @@ observerWaveText.observe(document.querySelector(".section-wave"));
 
 
 // Script for wave-animation
+// Moves the wave-background horizontally as you scroll. The 0.7 multiplier slows the wave, creating a smooth wave effect.
 const waveIcon = document.querySelector('.section-wave__icon');
 function handleWaveScroll() {
   const scrollPosition = window.pageYOffset;
@@ -31,7 +35,7 @@ window.addEventListener('scroll', handleWaveScroll);
 
 gsap.registerPlugin(ScrollTrigger); // Tells gsap to use scrolltrigger
 function horizontalScroll() {
-  gsap.set(".horizontal", { clearProps: "all" }); 
+  gsap.set(".horizontal", { clearProps: "all" });
   const scroll = document.querySelectorAll(".horizontal .horizontal__container"); // Finds elements inside .horizontal and .horizontal__container and puts them inside a variable
   let xPercentValue; // Sets a variable to store the amount percentage to move the cards horizontally.
   if (window.innerWidth >= 1000) { // This if statement will only work if the screensize is 1000px or more.
@@ -49,13 +53,12 @@ function horizontalScroll() {
       scrub: 1, // Make the scrolling to feel more smooth!
       end: "+=4000", // So the scrolling isn't so snappy!
     },
-      ease: "power1.inOut"
+    ease: "power1.inOut"
   });
 }
 horizontalScroll(); // Calls when the site is loaded
 window.addEventListener("resize", horizontalScroll);// If the window gets resized, run the horizontalScroll function again.
 // This is for if you are first on desktop and moves to phone, the function needs to be called again, or else the scrolling will be on phone size. 
-
 
 
 //gsap.registerPlugin(ScrollTrigger);
@@ -69,6 +72,7 @@ ScrollTrigger.create({
 
 
 // Button (Back to Top)
+// Scrolls the page to the top with a gradual animation using behavior: 'smooth' when clicked, avoiding an instant jump.
 const backToTop = document.querySelector('.message__btn');
 backToTop.onclick = function () {
   window.scrollTo({
